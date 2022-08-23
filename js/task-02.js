f03e31e3c1456c8771adeef2c2fa0362708a0a0c
Tonya-Shyva <toshash69@gmail.com>
2022-08-23 22:21:57 +0300

    result-hw-js-modul6

--- js/task-02.js
+++ js/task-02.js
@@ -1,18 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
-const ulFoodRef = document.getElementById('ingredients');
-const foodIngredients = ingredients.forEach(ingredient => {
-  let items = document.createElement('li');
-  items.innerHTML = ingredient;
-  items.classList.add('item');
-  ulFoodRef.append(items);
+const ulFoodRef = document.getElementById("ingredients");
+const foodIngredients = ingredients.forEach((ingredient) => {
+  let itemsEl = document.createElement("li");
+  itemsEl.innerHTML = ingredient;
+  itemsEl.classList.add("item");
+  ulFoodRef.append(itemsEl);
 });
 
 console.log(ulFoodRef);
