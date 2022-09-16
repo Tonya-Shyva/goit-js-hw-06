7a32bfaec5a9e61096aace9094d2098bd14350a5
Tonya-Shyva <toshash69@gmail.com>
2022-09-16 15:15:54 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,12 +7,37 @@ const ingredients = [
   "Condiments",
 ];
 
-const ulFoodRef = document.getElementById("ingredients");
-const foodIngredients = ingredients.forEach((ingredient) => {
-  let itemsEl = document.createElement("li");
-  itemsEl.innerHTML = ingredient;
-  itemsEl.classList.add("item");
-  ulFoodRef.append(itemsEl);
-});
+// -----  варіант зі старим методом createDocumentFragment----
 
-console.log(ulFoodRef);
+// const ulFoodRef = document.querySelector("#ingredients");
+// const fragmentLi = document.createDocumentFragment();
+// ingredients.forEach((ingredient) => {
+//   let item = document.createElement("li");
+//   item.textContent = ingredient;
+//   item.classList.add("item");
+//   fragmentLi.append(item);
+// });
+// ulFoodRef.append(fragmentLi);
+// console.log(ulFoodRef);
+
+// -------    Варіант з map  ------------------------
+
+// const ulFoodRef = document.querySelector("#ingredients");
+// const foodIngredients = ingredients.map((ingredient) => {
+//   let itemsEl = document.createElement("li");
+//   itemsEl.innerHTML = ingredient;
+//   itemsEl.classList.add("item");
+//   return itemsEl;
+// });
+
+// ulFoodRef.append(...foodIngredients);
+
+// ----------- з reduce ------------------------------
+
+const ulFoodRef = document.querySelector("#ingredients");
+
+const markupIngredients = ingredients.reduce(
+  (acc, ingredient) => acc + `<li class = "item"> ${ingredient} </li>`,
+  " "
+);
+ulFoodRef.insertAdjacentHTML("afterbegin", markupIngredients);
