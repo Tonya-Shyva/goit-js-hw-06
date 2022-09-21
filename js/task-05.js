e8b4e40eb611e18f375f034622ac5f7399542015
Tonya-Shyva <toshash69@gmail.com>
2022-09-21 09:01:36 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,15 @@ const nameOutputRef = document.querySelector("#name-output");
 inputRef.addEventListener("input", onInpytChange);
 
 function onInpytChange(event) {
-  if (inputRef.value === "") {
-    nameOutputRef.innerHTML = "Anonymous";
-  } else {
-    nameOutputRef.textContent = event.currentTarget.value;
-  }
+  // if (inputRef.value.trim() === "") {
+  //   nameOutputRef.innerHTML = "Anonymous";
+  // } else {
+  //   nameOutputRef.textContent = event.currentTarget.value.trim();
+  // }
+
+  // ----------- якщо тернарником----------------------------
+
+  inputRef.value.trim() === ""
+    ? (nameOutputRef.innerHTML = "Anonymous")
+    : (nameOutputRef.textContent = event.currentTarget.value.trim());
 }
